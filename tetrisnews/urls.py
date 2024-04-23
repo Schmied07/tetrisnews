0db1140db55c2e1d7b0759c9801172b2ac4a95c8
Thibaut TALOM KOYOUE <thibaut.talom.koyoue@efrei.net>
2024-04-23 16:11:36 +0200

    commit2

---
+++ tetrisnews/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.index,name='index'),
+    path('/legalprivacy',views.legalprivacy,name='legalprivacy'),
+    path('/termsofService',views.termsofService,name='termsofService')
+]
--- tetrisnews/views.py
+++ tetrisnews/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def  index(request):
+    context={"message":"Acceuil"}
+    template=loader.get_template("tetrisnews/index.html")
+    return HttpResponse(template.render(context,request))
+
+def  legalprivacy(request):
+    context={"message":"privacy-policy"}
+    template=loader.get_template("legal/privacy-policy.html")
+    return HttpResponse(template.render(context,request))
+
+def  termsofService(request):
+    context={"message":"Terms of Service"}
+    template=loader.get_template("legal/termsofService.html")
+    return HttpResponse(template.render(context,request))
