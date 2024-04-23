f14ceb9defcf6c1a319540dd579f3c831f9370d2
Schmied07 <talomthibaut@gmail.com>
2024-04-23 19:14:37 +0200

    Initial commit

--- tetrisnews/urls.py
+++ tetrisnews/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns=[
     path('',views.index,name='index'),
-    path('/legalprivacy',views.legalprivacy,name='legalprivacy'),
-    path('/termsofService',views.termsofService,name='termsofService')
+    path('legalprivacy',views.legalprivacy,name='legalprivacy'),
+    path('termsofService',views.termsofService,name='termsofService')
 ]
